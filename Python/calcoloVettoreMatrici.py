05f55ed81b9f5a0b2cc41bb54d9c9f84d1ea160a
Adamk93 <a.khayam93@gmail.com>
2016-06-02 15:40:52 +0200

    fixed state names

--- Python/calcoloVettoreMatrici.py
+++ Python/calcoloVettoreMatrici.py
@@ -78,74 +78,6 @@ def calcolo_matrice_osservazioni():
 
     return (matrice_O)
 
-"""def creazione_modello(matrice_T, matrice_O, vettore_Pi):
-
-    d1 = DiscreteDistribution({
-        'A':matrice_O[0,0], 'B':matrice_O[0,1], 'C':matrice_O[0,2], 'D':matrice_O[0,3], 'E':matrice_O[0,4],
-        'F':matrice_O[0,5], 'G':matrice_O[0,6], 'H':matrice_O[0,7], 'I':matrice_O[0,8], 'J':matrice_O[0,9],
-        'K':matrice_O[0,10], 'L':matrice_O[0,11], 'M':matrice_O[0,12], 'N':matrice_O[0,13], 'O':matrice_O[0,14],
-        'P':matrice_O[0,15], 'Q':matrice_O[0,16], 'R':matrice_O[0,17], 'S':matrice_O[0,18], 'T':matrice_O[0,19],
-        'U':matrice_O[0,20], 'V':matrice_O[0,21], 'W':matrice_O[0,22], "X":matrice_O[0,23], 'Y':matrice_O[0,24],
-        'Z':matrice_O[0,25], 'Space':matrice_O[0,26]})
-
-    d2 = DiscreteDistribution({
-        'A':matrice_O[1,0], 'B':matrice_O[1,1], 'C':matrice_O[1,2], 'D':matrice_O[1,3], 'E':matrice_O[1,4],
-        'F':matrice_O[1,5], 'G':matrice_O[1,6], 'H':matrice_O[1,7], 'I':matrice_O[1,8], 'J':matrice_O[1,9],
-        'K':matrice_O[1,10], 'L':matrice_O[1,11], 'M':matrice_O[1,12], 'N':matrice_O[1,13], 'O':matrice_O[1,14],
-        'P':matrice_O[1,15], 'Q':matrice_O[1,16], 'R':matrice_O[1,17], 'S':matrice_O[1,18], 'T':matrice_O[1,19],
-        'U':matrice_O[1,20], 'V':matrice_O[1,21], 'W':matrice_O[1,22], "X":matrice_O[1,23], 'Y':matrice_O[1,24],
-        'Z':matrice_O[1,25], 'Space':matrice_O[1,26]})
-
-    d3 = DiscreteDistribution({
-        'A':matrice_O[2,0], 'B':matrice_O[2,1], 'C':matrice_O[2,2], 'D':matrice_O[2,3], 'E':matrice_O[2,4],
-        'F':matrice_O[2,5], 'G':matrice_O[2,6], 'H':matrice_O[2,7], 'I':matrice_O[2,8], 'J':matrice_O[2,9],
-        'K':matrice_O[2,10], 'L':matrice_O[2,11], 'M':matrice_O[2,12], 'N':matrice_O[2,13], 'O':matrice_O[2,14],
-        'P':matrice_O[2,15], 'Q':matrice_O[2,16], 'R':matrice_O[2,17], 'S':matrice_O[2,18], 'T':matrice_O[2,19],
-        'U':matrice_O[2,20], 'V':matrice_O[2,21], 'W':matrice_O[2,22], "X":matrice_O[2,23], 'Y':matrice_O[2,24],
-        'Z':matrice_O[2,25], 'Space':matrice_O[2,26]})
-
-    #dichiaro gli stati
-    s1 = State(d1, name = "Stato Lettera A")
-    s2 = State(d2, name = "Stato Lettera B")
-    s3 = State(d3, name = "Stato Lettera C")
-
-    #aggiungo il modello e gli stati al modello
-    model = HiddenMarkovModel('HMM Mispelling')
-    model.add_states([s1, s2, s3])
-
-    #aggiungo transizioni tra stati
-    model.add_transition(model.start, s1, vettore_Pi.get('a'))
-    model.add_transition(model.start, s2, vettore_Pi.get('b'))
-    model.add_transition(model.start, s3, vettore_Pi.get('c'))
-
-    model.add_transition(s1, s1, matrice_T[0,0])
-    model.add_transition(s1, s2, matrice_T[0,1])
-    model.add_transition(s1, s3, matrice_T[0,2])
-    model.add_transition(s2, s1, matrice_T[1,0])
-    model.add_transition(s2, s2, matrice_T[1,1])
-    model.add_transition(s2, s3, matrice_T[1,2])
-    model.add_transition(s3, s1, matrice_T[2,0])
-    model.add_transition(s3, s2, matrice_T[2,1])
-    model.add_transition(s3, s3, matrice_T[2,2])
-
-    model.bake()
-
-    #predict - viterbi
-    matrix = model.forward(list('AAA'))
-    print("PREDICT")
-    print("matrix = ", matrix)
-
-    #viterbi algorithm
-    logp, path = model.viterbi(list('AAA'))
-    print("VITERBI")
-    print("logp = ", logp)
-    print("path = ", path)
-
-    #forward backward algorithm
-    emissions, transitions = model.forward_backward(list('CAB'))
-    print("FORWARD BACKWARD")
-    print("emissions = ", emissions)
-    print("transitions = ", transitions)"""
 
 def matrix_to_json(matrice):
     lista = list()
@@ -172,9 +104,9 @@ def creazione_modello(matrice_T, matrice_O, vettore_Pi):
     s = list()
     for j in range(0, 27):
         if(j != 26):
-            s.append(State(d[j], name = "Stato Lettera " + chr(j+97) ))
+            s.append(State(d[j], name = "" + chr(j+97) ))
         else:
-            s.append(State(d[j], name = "Stato Lettera Space" ))
+            s.append(State(d[j], name = "Space" ))
 
     model = HiddenMarkovModel('Prova')
     model.add_states(s)
