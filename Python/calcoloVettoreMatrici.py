30d283d56a42b60a0233438460ffe4990ef2eab1
Adamk93 <a.khayam93@gmail.com>
2016-06-03 20:03:58 +0200

    predictive capabilities issue completed

--- Python/calcoloVettoreMatrici.py
+++ Python/calcoloVettoreMatrici.py
@@ -7,6 +7,7 @@ from array import *
 from pomegranate import *
 import json
 import ast
+import editdistance
 
 def calcolo_vettore_pi():
     vettorePi = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0,
@@ -98,9 +99,8 @@ def matrix_to_json(matrice):
 
 
 def creazione_modello(matrice_T, matrice_O, vettore_Pi):
-    T = matrix_to_json(matrice_T)
+    #T = matrix_to_json(matrice_T)
     O = matrix_to_json(matrice_O)
-    print T
 
     dists = list()
     for i in range(0, 27):
@@ -130,23 +130,82 @@ def creazione_modello(matrice_T, matrice_O, vettore_Pi):
      
     model.bake()
     #model.draw()
+    return model
     
-
-    logp, path = model.viterbi(list("zs yoi see thrre ix a sea begween you abd mr"))
-    print("VITERBI")
+def delete__by_values(lst, values):
+    values_as_set = set(values)
+    return [ x for x in lst if x not in values_as_set ]
     
-    x=""    
+    
+def inferenza(model, sequence):
+    
+    logp, path = model.viterbi(sequence[0:len(sequence)])
+    #print("VITERBI")
+    
+    x=""
     for i in range(1, len(path)):
         if (json.loads(ast.literal_eval(json.dumps(str(path[i][1]), sort_keys= False))).get('name') == 'Space'):
             x = x+ " "
         else:
             x = x+""+json.loads(ast.literal_eval(json.dumps(str(path[i][1]), sort_keys= False))).get('name')
     
-    print x
+    #print x
+    return x
+   
+def test(model):
+    testing_sporchi = open("./PerturbazioneTweet/testing_sporchi.txt", "r")
+    testing_puliti = open("./PerturbazioneTweet/testing_puliti.txt", "r")
     
-# Chiamate delle funzioni che calcolano il vettore pi, la matrice T e la matrice O
+     
+    numchars = 0.0   
+    for line in testing_puliti.readlines():
+        print len(line)
+        for i in range(0, len(line)):
+            if(line[i].isalpha()):
+                numchars = numchars + 1.0
+    print numchars
+    
+    testing_puliti.close()
+    testing_puliti = open("./PerturbazioneTweet/testing_puliti.txt", "r")    
+    lines_pulite=testing_puliti.readlines()
+    
+    unlist = ['\x82', '\xac','\x87', '\xbd', '\xbe', '\xb6', '\xa4', '\xc5', '\x9f', '\xc4', '\xb1', '\xc3', '\xbc', '\xa3', '$', '\x98', '%', '\xa6', '\x9c', '\x9d', '|', ']', '[', '_', '\xc2', '\xa0', '\x99', ';', '+', '=', '*', '\xe2', '\x80', '\x94','?', '\n', ':', '\'', '/', '!', ',', '.', '-', '"', '(', ')', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    numLine=0   
+    editDistance = 0.0
+    for line in testing_sporchi.readlines(): 
+        x=''.join(delete__by_values(list(line.lower()), unlist))
+        y = ''.join(delete__by_values(lines_pulite[numLine].lower(), unlist))
+        
+        newL=len(x)-1      
+        j = 0  
+        for i in range(0, len(x)-1):
+            if(newL > i):
+                if(x[j].isspace() and x[j + 1].isspace()):                    
+                    x = x[0:j]+x[j+2:len(x)]
+                    y = y[0:j]+y[j+2:len(y)]
+                    newL = newL -1
+                else:
+                    j = j + 1
+                   
+        x = inferenza(model, list(x))        
+                                        
+        editDistance += editdistance.eval(y, x)
+        numLine += 1
+    
+    testing_sporchi.close()
+    testing_puliti.close()
+    print editDistance
+    return (editDistance/numchars)
+   
+    
+    
+# Pipeline di trattamento dati e inferenza!
 
 vettore_Pi = calcolo_vettore_pi()
 matrice_T = calcolo_matrice_transizioni()
 matrice_O = calcolo_matrice_osservazioni()
-creazione_modello(matrice_T, matrice_O, vettore_Pi)
+model = creazione_modello(matrice_T, matrice_O, vettore_Pi)
+#inferenza(model, list("zs yoi see thwre ix a sea begween you abd mr"))
+#inferenza(model, list("qe zll ljvr ih tue ywllow xubmarine"))
+errore = test(model)
+print errore
